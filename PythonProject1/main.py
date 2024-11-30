7baae97f95cd2b1920ec6d07a4a3bbf495ae5f66
alexharrod <alexkharrod@gmail.com>
2024-11-30 11:02:09 -0500

    Final

--- PythonProject1/main.py
+++ PythonProject1/main.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+import smtplib
+
+from flask import Flask, render_template, request
 import requests
+from smtplib import SMTP
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+EMAIL = "alexkharrodpython"
+SMTP_URL = "smtp.gmail.com"
+PASSWORD = os.getenv("PASSWORD")
+
+
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
@@ -17,9 +32,17 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'GET':
+        return render_template("contact.html")
+    elif request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        send_mail(address=email, message=message, name=name, phone=phone)
+        return f"<h1>Hello {name}, we successfully sent your message"
 
 
 @app.route("/post/<int:index>")
@@ -29,6 +52,14 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+def send_mail(address, message, name, phone):
+    with smtplib.SMTP(host=SMTP_URL, port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL, to_addrs=address, msg=f"Subject: Contact Request from {name}\n\nName: {name}\nPhone: {phone}"
+                                                                   f"\nEmail: {address}\n Message: {message}")
+
 
 
 if __name__ == "__main__":
